f4bf08f6f140a84b25f0a920b739d93b98cb7d09
村田光 <your.muhi_1122@icloud.com>
2022-02-02 02:06:05 +0900

    バリデーションの設定

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
-
+  validates :name, presence: true
+  validates :deadline, presence: true
 
   def complete
     self.is_finished = true
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :tasks
+  has_many :tasks, dependent: :destroy
   validates :name,  presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
